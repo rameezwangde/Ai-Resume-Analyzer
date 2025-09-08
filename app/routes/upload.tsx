149f2652b1fdfabb3d7a46cb1569a981f30bd855
rameez <rameezwang@gmail.com>
2025-09-08 23:49:39 +0530

    Partial Upload

---
+++ app/routes/upload.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {useState} from 'react'
+import Navbar from "~/components/Navbar";
+const upload = () => {
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+
+        <section className="main-section">
+            <div className="page-heading">
+                <h1>Smart feedback for your dream job</h1>
+               
+                
+            </div>
+        </section>
+    </main>
+  )
+}
+
+export default upload

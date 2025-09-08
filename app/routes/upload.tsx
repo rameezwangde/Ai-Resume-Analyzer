2d500bcc611ea0864f8007dea9473a4c1a3a138d
rameez <rameezwang@gmail.com>
2025-09-09 00:42:49 +0530

    partial upload done

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {useState} from 'react'
 import Navbar from "~/components/Navbar";
 const upload = () => {
+    const[isProcessing,setIsProcessing]=useState(false);
+    const[statusText,setStatusText]=useState();
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
         <Navbar/>
@@ -9,7 +11,14 @@ const upload = () => {
         <section className="main-section">
             <div className="page-heading">
                 <h1>Smart feedback for your dream job</h1>
-               
+                {isProcessing ? (
+                        <>
+                            <h2>{statusText}</h2>
+                            <img src="/images/resume-scan.gif" className="w-full" />
+                        </>
+                    ) : (
+                        <h2>Drop your resume for an ATS score and improvement tips</h2>
+                    )}
                 
             </div>
         </section>
